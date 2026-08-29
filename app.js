7dcad6b64e6e4acdb63e9bd9b452f5377f638d5c
cjy0281 <chiang0281@gmail.com>
2026-08-29 21:59:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ const resources = [
     status: '開啟圖片'
   },
   { name: '32 Starters 總表', href: '32Starters.pdf', status: '開啟總表'},
-  { name: '學習單'  },
+  { name: '教材講義', href: 'Teaching materials.pdf', status: '開啟檔案' },
   
   { name: '臺灣學術倫理教育資源中心' , href: 'https://ethics.moe.edu.tw/', status: '開啟網站'  },
 
